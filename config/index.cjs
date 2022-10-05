a404408508cd529718e5906dc178b4c6fdf1da8e
lanbin <2602808039@qq.com>
2022-10-05 20:32:51 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -42,7 +42,7 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-05-14' },
+        { keyword: 'love_day', date: '2022-08-09' },
         // 结婚纪念日
        // { keyword: 'marry_day', date: '2022-09-09' },
       ],
