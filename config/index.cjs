60cfa2054857acda24a9119e9d90cc6c2318c0bc
lanbin <2602808039@qq.com>
2022-10-05 20:23:55 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,30 +21,30 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '管管',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'op0Rn6yW6ZaooWErt2Ey4i095h3w',
+      id: 'op0Rn61gzlra8c-Pa0QkpQHaayfA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'G9v3VQBDk6zgsdsRDwJNQhc1kA3M6JHbl7H1uz8jp7o',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '05-19',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '管也阴历', year: '1996', date: '04-03',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '管也阳历', year: '1996', date: '05-19',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2022', date: '05-14',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2022-05-14' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+       // { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],
