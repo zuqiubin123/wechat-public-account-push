720bb4252e6c6a0b8038eed551f813d8c6e2d5f6
lanbin <2602808039@qq.com>
2022-10-24 21:36:11 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -29,11 +29,11 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '乖乖阴历', year: '1997', date: '08-19',
+          type: '*生日', name: '乖乖阴历', year: '1997', date: '07-17',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '乖乖阳历', year: '1997', date: '07-17',
+          type: '生日', name: '乖乖阳历', year: '1997', date: '08-19',
         },
         {
           type: '节日', name: '相识纪念日', year: '2022', date: '05-14',
