2b6e2d28db12f0dc2ef08266eaad9325283b6a19
zuqiubin123 <114671450+zuqiubin123@users.noreply.github.com>
2023-05-17 22:54:35 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'op0Rn69YXmIX82E0l3BBjyh8_SF0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'Wz0r28LITXFg_S8_PYrfdFrzx_m7z9un6IOknWDJz1o',
+      useTemplateId: 'sZVKe6t2OWXvdqXaJUbd2HZ_2WIylND1tmVLf4AzovM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-19',
       festivals: [
